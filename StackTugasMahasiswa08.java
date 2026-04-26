f815970a5c9e15340c9a915f74fe4404b5f88243
kamilakhairunnisa <erinazkk@gmail.com>
2026-04-26 10:29:15 +0700

    Modifikasi StacyTugasMahasiswa

---
+++ StackTugasMahasiswa08.java
@@ -0,0 +1,75 @@
+public class StackTugasMahasiswa08 {
+    Mahasiswa08[] stack;
+    int size;
+    int top;
+
+    public StackTugasMahasiswa08(int size) {
+        this.size = size;
+        stack = new Mahasiswa08[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa08 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa08 pop() {
+        if (!isEmpty()) {
+            Mahasiswa08 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa08 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    public void print() {
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+
+    public Mahasiswa08 lihatBawah() {
+    if (!isEmpty()) {
+        return stack[0];
+    } else {
+        System.out.println("Stack kosong!");
+        return null;
+    }
+}
+    public int jumlahTugas() {
+    return top + 1;
+}
+}
