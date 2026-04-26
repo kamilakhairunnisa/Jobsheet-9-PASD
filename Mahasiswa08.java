821174bd6651c2eabd2bb8dbf6306c0a35e2f596
kamilakhairunnisa <erinazkk@gmail.com>
2026-04-26 08:23:43 +0700

    Percobaan1

---
+++ Mahasiswa08.java
@@ -0,0 +1,19 @@
+public class Mahasiswa08 {
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    public Mahasiswa08() {}
+
+    public Mahasiswa08(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
